fdf6b3ce2a9d7a2911270efb915a44dcb2c317f8
Serhii Mikheiev <mikh.s.o@gmail.com>
2021-03-08 01:01:12 +0200

    add input form

--- react-native.keyboard-aware-scroll-view-test/App.tsx
+++ react-native.keyboard-aware-scroll-view-test/App.tsx
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={StyleSheet.absoluteFill}>
+        <ScrollView>
+          <View style={styles.container}>
+            <View style={styles.formControl}>
+              <Text style={styles.inputLabel}>Username</Text>
+              <TextInput style={styles.textInput}></TextInput>
+            </View>
+            <View style={styles.formControl}>
+              <Text style={styles.inputLabel}>Password</Text>
+              <TextInput style={styles.textInput}></TextInput>
+            </View>
+            <View style={styles.formControl}>
+              <Text style={styles.inputLabel}>Email</Text>
+              <TextInput style={styles.textInput}></TextInput>
+            </View>
+            <View style={styles.formControl}>
+              <Text style={styles.inputLabel}>Phone</Text>
+              <TextInput style={styles.textInput}></TextInput>
+            </View>
+            <View style={styles.formControl}>
+              <Text style={styles.inputLabel}>City</Text>
+              <TextInput style={styles.textInput}></TextInput>
+            </View>
+            <View style={styles.formControl}>
+              <Text style={styles.inputLabel}>Address</Text>
+              <TextInput style={styles.textInput}></TextInput>
+            </View>
+          </View>
+        </ScrollView>
+        <View style={styles.buttonContainer}>
+          <Button onPress={() => {}} title="Submit" color="#841584" />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    justifyContent: "flex-start",
+  },
+  formControl: {
+    marginBottom: 20,
+    padding: 10,
+    width: "100%",
+  },
+  inputLabel: {
+    fontSize: 30,
+    paddingBottom: 10,
+  },
+  textInput: {
+    borderColor: "gray",
+    borderWidth: 1,
+    fontSize: 24,
+    height: 50,
+  },
+  buttonContainer: {
+    justifyContent: "center",
+    height: 80,
+    borderWidth: 1,
   },
 });
