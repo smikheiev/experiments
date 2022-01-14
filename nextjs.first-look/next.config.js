10528bc60277ca1bad428bc3a34a3a655d197d10
Serhii Mikheiev <mikh.s.o@gmail.com>
2022-01-14 22:03:42 +0200

    deploy to firebase

--- nextjs.first-look/next.config.js
+++ nextjs.first-look/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "./dist/next",
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
