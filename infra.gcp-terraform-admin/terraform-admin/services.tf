81c4b6c6f4888ebbcc3bdfd3038a9272b3ee7348
Serhii Mikheiev <mikh.s.o@gmail.com>
2024-06-04 23:04:43 +0300

    add missing serviceusage service

--- infra.gcp-terraform-admin/terraform-admin/services.tf
+++ infra.gcp-terraform-admin/terraform-admin/services.tf
@@ -2,3 +2,8 @@ resource "google_project_service" "cloudbilling" {
   project = google_project.terraform-admin.project_id
   service = "cloudbilling.googleapis.com"
 }
+
+resource "google_project_service" "serviceusage" {
+  project = google_project.terraform-admin.project_id
+  service = "serviceusage.googleapis.com"
+}
