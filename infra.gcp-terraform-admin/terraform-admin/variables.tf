5b89c6ab370813f0640a56790676ab97595e139c
Serhii Mikheiev <mikh.s.o@gmail.com>
2024-06-04 16:34:23 +0300

    add initial terraform-admin project

---
+++ infra.gcp-terraform-admin/terraform-admin/variables.tf
@@ -0,0 +1,19 @@
+variable "billing_account_id" {
+  type = string
+}
+
+variable "folder_id" {
+  type = string
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
