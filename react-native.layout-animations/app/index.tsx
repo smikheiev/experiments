af15255e17e026950b41150a1c91995fad6fe0e6
Serhii Mikheiev <mikh.s.o@gmail.com>
2024-08-08 12:49:12 +0300

    add fab to add new items

--- react-native.layout-animations/app/index.tsx
+++ react-native.layout-animations/app/index.tsx
@@ -1,35 +1,60 @@
 import { useState } from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function HomeScreen() {
-  const [items, _] = useState(
+  const [items, setItems] = useState(
     new Array(20).fill(0).map((_, index) => ({ id: index }))
   );
 
+  const addItem = () => {
+    setItems((items) => {
+      const lastItem = items[items.length - 1];
+      return [...items, { id: (lastItem?.id ?? 0) + 1 }];
+    });
+  };
+
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        flexWrap: "wrap",
-        gap: 16,
-        padding: 16,
-      }}
-    >
-      {items.map((item) => (
-        <View
-          key={item.id}
-          style={{
-            padding: 16,
-            borderWidth: 1,
-            borderColor: "black",
-            borderRadius: 16,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Text style={{ fontSize: 24 }}>{item.id}</Text>
-        </View>
-      ))}
-    </View>
+    <>
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: 16,
+          padding: 16,
+        }}
+      >
+        {items.map((item) => (
+          <View
+            key={item.id}
+            style={{
+              padding: 16,
+              borderWidth: 1,
+              borderColor: "black",
+              borderRadius: 16,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ fontSize: 24 }}>{item.id}</Text>
+          </View>
+        ))}
+      </View>
+      <Pressable
+        style={{
+          backgroundColor: "black",
+          width: 52,
+          aspectRatio: 1,
+          borderRadius: 16,
+          position: "absolute",
+          bottom: 32,
+          right: 32,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        onPress={addItem}
+      >
+        <Text style={{ color: "white", fontSize: 32 }}>+</Text>
+      </Pressable>
+    </>
   );
 }
