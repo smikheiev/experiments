ac7645711d05d01a12c45ea0d7b36b990e890c81
Serhii Mikheiev <mikh.s.o@gmail.com>
2024-08-08 12:46:12 +0300

    add a simple predefined list of items

--- react-native.layout-animations/app/index.tsx
+++ react-native.layout-animations/app/index.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const [items, _] = useState(
+    new Array(20).fill(0).map((_, index) => ({ id: index }))
+  );
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello World</Text>
+    <View
+      style={{
+        flexDirection: "row",
+        flexWrap: "wrap",
+        gap: 16,
+        padding: 16,
+      }}
+    >
+      {items.map((item) => (
+        <View
+          key={item.id}
+          style={{
+            padding: 16,
+            borderWidth: 1,
+            borderColor: "black",
+            borderRadius: 16,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ fontSize: 24 }}>{item.id}</Text>
+        </View>
+      ))}
     </View>
   );
 }
